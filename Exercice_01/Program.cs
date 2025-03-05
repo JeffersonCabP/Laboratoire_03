cbde451e2b70c118b7a7b74b7d748fa3467c8289
JeffersonCabP <2132486@cstj.qc.ca>
2025-03-05 01:45:22 -0500

    Update Program.cs

--- Exercice_01/Program.cs
+++ Exercice_01/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Valeurs nouvelleValeur = new Valeurs(); // Fait appel au constructeur par défaut
-            nouvelleValeur.LectureFichier(); // Fait appel à la lecture du fichier pour charger les dobbées dans nos listes
+            nouvelleValeur.LectureFichier(); // Fait appel à la lecture du fichier pour charger les données dans nos listes
             nouvelleValeur.CalculerVitesse(); // Fait appel à la méthode pour complèter la liste de vitesse
             nouvelleValeur.EcritureFichier(); // Fait appel à la méthode pour sauvegarder dans le fichier
         }
